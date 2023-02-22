0390ec42e722daab5427adf1e9215b6fb6a7b0a5
Jakub <hh5442138@gmail.com>
2023-02-22 11:38:01 +0100

    Minecraft-Projekt v1.2

--- minecraft.py
+++ minecraft.py
@@ -29,24 +29,42 @@ for x in range(20):
   for y in range(1):
     for z in range(20):
      add_box( (x, y, z) )
-
-def add_Bedrock(position):
+     
+     def add_box(position):
+      boxes.append(
         Button(
         parent=scene,
         model='cube',
         origin=0.5,
-        color=color.black,
-        position=position
+        color=random_color(),
+        position=position,
+        texture='grass'
       )
-for x in range(20):
-         for z in range(20):
-          add_Bedrock( (x, -1, z) )    
+    )
+
+for x in range(19):
+    for z in range(19):
+     add_box( (x, -1, z) )
+
+     def add_box(position):
+      boxes.append(
+        Button(
+        parent=scene,
+        model='cube',
+        origin=0.5,
+        color=random_color(),
+        position=position,
+        texture='grass'
+      )
+    )
+
+for x in range(18):
+    for z in range(18):
+     add_box( (x, -2, z) )   
+
      
-     
-     
-     
-def input(key):
-    for box in boxes:
+     def input(key):
+       for box in boxes:
         if box.hovered:
             if key == "left mouse down":
                 add_box(box.position + mouse.normal)
