18bfdd0f12bff8d0f9316b2e939a9dd7fb3f3198
Jakub <hh5442138@gmail.com>
2023-03-19 17:01:55 +0100

    Revert "Minecraft-Projekt v1.2"
    This reverts commit 0390ec42e722daab5427adf1e9215b6fb6a7b0a5.

--- minecraft.py
+++ minecraft.py
@@ -29,42 +29,24 @@ for x in range(20):
   for y in range(1):
     for z in range(20):
      add_box( (x, y, z) )
-     
-     def add_box(position):
-      boxes.append(
+
+def add_Bedrock(position):
         Button(
         parent=scene,
         model='cube',
         origin=0.5,
-        color=random_color(),
-        position=position,
-        texture='grass'
+        color=color.black,
+        position=position
       )
-    )
-
-for x in range(19):
-    for z in range(19):
-     add_box( (x, -1, z) )
-
-     def add_box(position):
-      boxes.append(
-        Button(
-        parent=scene,
-        model='cube',
-        origin=0.5,
-        color=random_color(),
-        position=position,
-        texture='grass'
-      )
-    )
-
-for x in range(18):
-    for z in range(18):
-     add_box( (x, -2, z) )   
-
+for x in range(20):
+         for z in range(20):
+          add_Bedrock( (x, -1, z) )    
      
-     def input(key):
-       for box in boxes:
+     
+     
+     
+def input(key):
+    for box in boxes:
         if box.hovered:
             if key == "left mouse down":
                 add_box(box.position + mouse.normal)
